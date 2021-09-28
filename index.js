10c0c5e4ab452dae0b6b1474fba2056a858d0e84
ShawnTownsend <stitans28@gmail.com>
2021-09-28 18:23:40 +0000

    Third Function

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function multiplesOfThree() {
         }
     }
 }
+
+//3
+const multiplesOfThreeOrFive = () => {
+    for(let i = 1; i < 101; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
+}
