1b7771f6f2501f3a624afa00a8a154e23b4bbf15
Matthew Tan <tanmatt804@gmail.com>
2021-09-28 18:17:40 +0000

    second function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 //Code your solutions in this file
+//1
 function fiveToOneHundred() {
     for(let i = 5; i <= 100; i++) {
         console.log(i);
+    }
+}
+
+//2
+function multiplesOfThree() {
+    for(let i = 1; i<=100; i++){
+        if(i % 3 == 0) {
+            console.log(i)
+        }
     }
 }
